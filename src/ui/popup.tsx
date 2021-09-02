0d2ff70cd06cebcb11fce188d1a622c3c3e622b3
l0x539 <nogues322@gmail.com>
2021-09-02 23:38:27 +0100

    Styling with bootstrap 5.1.0

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -2,25 +2,51 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/popup.css"
+import { Alert, Button, Form, FormControl, InputGroup } from "react-bootstrap";
 
-class Hello extends React.Component {
+class App extends React.Component {
     render() {
         return (
-            <div className="popup-padded">
-                <form className="form-horizontal" action="/en/signin" accept-charset="UTF-8" method="post">
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="session_email">Email</label>
-                        <input className="form-control" type="text" name="session[email]"/>
+            <div className="popup-padded container">
+                <div className="row">
+                    <div className="col-lg-6 d-flex signupbox_hint">
+                        <div>
+                        <h1>Sign in</h1>
+                        <section className="short-explain">
+                            Sign in to use all features of ZEIT.IO
+                        </section>
+                        </div>    
                     </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="session_password">Password</label>
-                        <input className="form-control" type="password" name="session[password]" />
-                        <span className="pwdreset"><a href="/en/iforgotmypassword">Password reset</a></span>
+                    <div className="col-lg-6 shadow-sm signupbox">
+                        <Alert variant={"danger"} dismissible>
+                            Error goes here
+                        </Alert>
+                        <Form className="form-horizontal" action="/en/signin" method="post">
+                            <Form.Label className="form-label" htmlFor="session_email">Email</Form.Label>
+                            <div className="mb-3">
+                                <FormControl
+                                aria-label="Username"
+                                id="session_email"
+                                aria-describedby="basic-addon1"
+                                type="email"
+                                />
+                            </div>
+                            <Form.Label className="form-label" htmlFor="session_password">Password</Form.Label>
+                            <div className="mb-3">
+                                <FormControl
+                                aria-label="Username"
+                                id="session_password"
+                                aria-describedby="basic-addon1"
+                                type="password"
+                                />
+                                <span className="pwdreset"><a href="/en/iforgotmypassword">Password reset</a></span>
+                            </div>
+                            <div className="actions">
+                                <Button name="commit" >Sign in</Button>
+                            </div>
+                        </Form>
                     </div>
-                    <div className="actions">
-                        <input type="submit" name="commit" value="Sign in" className="btn btn-primary" data-disable-with="Sign in" />
-                    </div>
-                </form>
+                </div>
             </div>
         )
     }
@@ -29,6 +55,6 @@ class Hello extends React.Component {
 // --------------
 
 ReactDOM.render(
-    <Hello />,
+    <App />,
     document.getElementById('root')
 )
