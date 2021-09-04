f5355726cbbe9424f3bbe07036efefab6913a93f
l0x539 <nogues322@gmail.com>
2021-09-04 03:45:25 +0100

    Close login error.

--- src/ui/pages/Login.tsx
+++ src/ui/pages/Login.tsx
@@ -46,7 +46,7 @@ const Login = () => {
                 </div>
                 <div className="col-lg-6 shadow-sm signupbox">
                     {error.length > 0 ? 
-                        <Alert variant={"danger"} onClose={() => setError('false')} dismissible>
+                        <Alert variant={"danger"} onClose={() => setError('')} dismissible>
                             {error}
                         </Alert>
                     :
