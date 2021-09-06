d760145b0c491fdb6b18d89611d82981f47a9170
l0x539 <nogues322@gmail.com>
2021-09-06 23:47:35 +0100

    Start/Pause browser shortcut.

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,3 +1,5 @@
+import {request} from '../ui/utils/api';
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log('Background got a message!');
   sendResponse({});
@@ -7,6 +9,37 @@ chrome.runtime.onConnect.addListener(function(port) {
   if (port.name === 'Zeit') {
     port.onDisconnect.addListener(function() {
       chrome.runtime.reload();
+    });
+  }
+});
+
+const startStop = () => {
+  chrome.storage.local.get('apiKey', async function(result) {
+    const start = await request('/api/v1/usr/time_records/start', 'POST',
+        {},
+        result.apiKey,
+    );
+    if (start.error || start.message === 'Timer was started already') {
+      const pause = await request('/api/v1/usr/time_records/pause', 'POST',
+          {},
+          result.apiKey,
+      );
+      if (pause.error || pause.message === 'Timer was paused already') {
+        await request('/api/v1/usr/time_records/resume', 'POST',
+            {},
+            result.apiKey,
+        );
+      }
+    }
+  });
+};
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'start-stop') {
+    chrome.storage.local.get('settings', (results) => {
+      if (results?.settings?.startStop) {
+        startStop();
+      }
     });
   }
 });
