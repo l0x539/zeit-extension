1e6b259d86ab26c6eb3ca94fb7e48da9f1f7ea82
l0x539 <nogues322@gmail.com>
2022-01-29 15:11:02 +0100

    Adding safari build command.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = (env) => ({
 
   output: {
     path: path.resolve(__dirname, env?.browser === 'firefox' ?
-        'firefox-dist/js' : 'dist/js'),
+        'firefox-dist/js' : env?.browser === 'safari' ?
+        'safari-dist/js' : 'dist/js'),
     filename: '[name].js',
   },
 
