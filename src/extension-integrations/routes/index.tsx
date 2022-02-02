5447f9a8f949f3c7e018baf2aa88b3e37b5bc192
Star Neit <starneit105@gmail.com>
2022-02-02 21:41:37 +0100

    Update

--- src/extension-integrations/routes/index.tsx
+++ src/extension-integrations/routes/index.tsx
@@ -10,7 +10,9 @@ export const ROUTES = [
   {
     integration: 'jira',
     rules: [
-      /^https\:\/\/.*atlassian\.net\/.*\/boards\/.?\?.*selectedIssue=.*$/,
+      /^https\:\/\/.*atlassian\.net\/.*\/boards\/.*\?.*selectedIssue=.*$/,
+      /^https\:\/\/.*atlassian\.net\/browse\/.+/,
+      /^https\:\/\/jira\.jda\.com\/browse\/.+/
     ],
   },
   {
